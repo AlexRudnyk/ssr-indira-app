d1c2be49ca05caedf84a25ec92a8dfb1a721472d
AlexRudnyk <alexander.rudnyk@ukr.net>
2024-12-22 16:06:28 +0200

    added control buttons to a product page

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import { FC } from "react"
+import { Button } from "@mui/material"
 import Image from "next/image"
+import Link from "next/link"
 
 import s from "./ProductPage.module.scss"
 
+import routes from "@/helpers/routes"
 import { useQueryProduct } from "@/hooks/useQueryProducts"
 
 type Props = {
@@ -25,11 +28,33 @@ const ProductPage: FC<Props> = ({ id }) => {
             height={400}
             className={s.productImage}
           />
-          <div className={s.descriptionWrapper}>
-            <h3>{product.title}</h3>
-            <p>{product.text}</p>
-            <p>{product.description}</p>
-            <p>Price: {product.price} UAH</p>
+          <div className={s.descAndBtnsWrapper}>
+            <div className={s.descriptionWrapper}>
+              <h3>{product.title}</h3>
+              <p>{product.text}</p>
+              <p>{product.description}</p>
+              <p>Price: {product.price} UAH</p>
+            </div>
+            <div className={s.btnsContainer}>
+              <div className={s.btnWrapper}>
+                <Button type="button" variant="contained">
+                  Add to Cart
+                </Button>
+                <Link href={routes.home}>
+                  <Button type="button" variant="contained">
+                    Back to Home
+                  </Button>
+                </Link>
+              </div>
+              <div className={s.btnWrapper}>
+                <Button type="button" variant="outlined">
+                  Comments
+                </Button>
+                <Button type="button" variant="outlined">
+                  Add Comment
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>

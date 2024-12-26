51dc5823dc3009caab8aad0db6923f63bf4b7f9a
AlexRudnyk <alexander.rudnyk@ukr.net>
2024-12-26 17:48:38 +0200

    added total order sum count

--- src/components/CartPage/CartPage.tsx
+++ src/components/CartPage/CartPage.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import Link from "next/link"
 
 import CartProductsList from "../CartProductsList"
@@ -13,7 +14,20 @@ import { useAuth } from "@/hooks/useAuth"
 const CartPage = () => {
   const { isLoggedIn, user } = useAuth()
   const { cart } = useGlobalContext()
+  const [totalSum, setTotalSum] = useState<number>(0)
+  const [totalSumDB, setTotalSumDB] = useState<number>(0)
 
+  const calculateTotal = (products: { price: number; quantity: number }[] | undefined): number => {
+    return products?.reduce((acc, product) => acc + product.price * product.quantity, 0) || 0
+  }
+
+  useEffect(() => {
+    setTotalSum(calculateTotal(cart))
+  }, [cart])
+
+  useEffect(() => {
+    setTotalSumDB(calculateTotal(user?.productsInCart))
+  }, [user?.productsInCart])
   const isEmptyCart = isLoggedIn ? user?.productsInCart.length === 0 : cart.length === 0
 
   return (
@@ -26,10 +40,14 @@ const CartPage = () => {
           </Link>
         </div>
       ) : (
-        <>
+        <div className={s.cartProductListWrapper}>
           <h3 className={s.title}>Products in your Cart</h3>
           <CartProductsList cart={isLoggedIn ? user?.productsInCart : cart} />
-        </>
+          <p>
+            Your order total amount is:{" "}
+            <span className={s.totalSum}>{isLoggedIn ? totalSumDB : totalSum}</span> UAH
+          </p>
+        </div>
       )}
     </div>
   )
